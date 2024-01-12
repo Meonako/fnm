f111935d6e81d1b0b46755c1c5be4da144b7fbaf
Meonako <dubi.db28@gmail.com>
2024-01-12 15:08:07 +0700

    Feat: `list-remote` not print table instead of long ass line feed
    Add: `profile.release` to `Cargo.toml`
    
    Remove: unused use directive

--- src/commands/ls_remote.rs
+++ src/commands/ls_remote.rs
@@ -1,6 +1,9 @@
+use std::io::{stdout, Write};
+
 use crate::config::FnmConfig;
 use crate::remote_node_index;
 use thiserror::Error;
+use comfy_table::{Table, Cell};
 
 #[derive(clap::Parser, Debug)]
 pub struct LsRemote {}
@@ -9,15 +12,29 @@ impl super::command::Command for LsRemote {
     type Error = Error;
 
     fn apply(self, config: &FnmConfig) -> Result<(), Self::Error> {
+        let mut stdout = stdout();
+        print!("Fetching all versions...\r");
+        stdout.flush().unwrap();
         let all_versions = remote_node_index::list(&config.node_dist_mirror)?;
 
-        for version in all_versions {
-            print!("{}", version.version);
-            if let Some(lts) = &version.lts {
-                print!(" ({lts})");
+        let mut table = Table::new();
+        table.load_preset(comfy_table::presets::UTF8_FULL_CONDENSED).set_content_arrangement(comfy_table::ContentArrangement::DynamicFullWidth);
+        let mut a_row = Vec::new();
+        for version in all_versions.into_iter().rev().take(60) {
+            let formatted = if let Some(lts) = &version.lts {
+                format!("{} ({lts})", version.version)
+            } else {
+                format!("{}", version.version)
+            };
+            
+            a_row.push(Cell::new(formatted).set_alignment(comfy_table::CellAlignment::Center));
+
+            if a_row.len() == 3 {
+                table.add_row(a_row);
+                a_row = Vec::new();
             }
-            println!();
         }
+        println!("{table}");
 
         Ok(())
     }
